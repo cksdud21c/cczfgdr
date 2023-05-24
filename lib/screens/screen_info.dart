168adad50d88ef9ff9c8c65a058074859b3e6c2e
cksdud21c <127474805+cksdud21c@users.noreply.github.com>
2023-05-24 12:29:00 +0900

    Add files via upload

---
+++ lib/screens/screen_info.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  String? old;
+  String? sex;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  void fetchUserData() async {
+    try {
+      // Make an HTTP request to fetch the user data from the server
+      final response = await http.get(Uri.parse('http://your-api-url/user-data'));
+
+      if (response.statusCode == 200) {
+        // Parsing the response data and update the state
+        final data = json.decode(response.body);
+        setState(() {
+          old = data['old'];
+          sex = data['sex'];
+        });
+      } else {
+        // Handle error response
+        print('Failed to fetch user data: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle network or server error
+      print('Failed to fetch user data: $e');
+    }
+  }
+
+  void saveUserData() async {
+    try {
+      // Construct the request body with the updated values
+      final data = {'old': old, 'sex': sex};
+      final body = json.encode(data);
+      final url = Uri.parse('http://your-api-url/save-user-data');
+      // Make an HTTP post request to save the user data on the server
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        // Show a SnackBar with the "저장!" message
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('저장!'),
+          ),
+        );
+        // Close the page
+        Navigator.pop(context);
+      } else {
+        // Handle error response
+        print('Failed to save user data: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle network or server error
+      print('Failed to save user data: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 6, 67, 117),
+        title: Text('회원 정보 수정'),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            OldInput(
+              initialValue: old,
+              onChanged: (value) {
+                setState(() {
+                  old = value;
+                });
+              },
+            ),
+            SexInput(
+              initialValue: sex,
+              onChanged: (value) {
+                setState(() {
+                  sex = value;
+                });
+              },
+            ),
+            ElevatedButton(
+              onPressed: saveUserData,
+              child: Text('저장'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SexInput extends StatelessWidget {
+  final String? initialValue;
+  final ValueChanged<String>? onChanged;
+
+  const SexInput({Key? key, this.initialValue, this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _sex = ['남성', '여성'];
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: DropdownButtonFormField(
+        value: initialValue,
+        items: _sex.map(
+              (e) => DropdownMenuItem(value: e, child: Text(e)),
+        ).toList(),
+        onChanged: onChanged != null ? (String? value) => onChanged!(value!) : null,
+        decoration: InputDecoration(
+          labelText: "성별",
+        ),
+      ),
+    );
+  }
+}
+
+
+class OldInput extends StatelessWidget {
+  final String? initialValue;
+  final ValueChanged<String>? onChanged;
+
+  const OldInput({Key? key, this.initialValue, this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        onChanged: onChanged,
+        keyboardType: TextInputType.number,
+        controller: TextEditingController(text: initialValue),
+        decoration: InputDecoration(
+          labelText: "나이",
+        ),
+      ),
+    );
+  }
+}
